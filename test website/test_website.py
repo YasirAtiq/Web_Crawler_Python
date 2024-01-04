65168c4cd6ce37be36ec1fcc8cf7aae2993c311e
Yasir <DPSA00609@mypaceschool.com>
2024-01-04 16:54:39 +0400

    Making website redirections complex.

--- test website/test_website.py
+++ test website/test_website.py
@@ -7,6 +7,20 @@ app = flask.Flask(__name__)
 def home():
     return flask.render_template("home.html")
 
+@app.route("/a")
+def page_a():
+    return flask.render_template("a.html")
+
+
+@app.route("/b")
+def page_b():
+    return flask.render_template("b.html")
+
+
+@app.route("/c")
+def page_c():
+    return flask.render_template("c.html")
+
 
 if __name__ == "__main__":
     app.run("localhost", 8000, debug=True)
